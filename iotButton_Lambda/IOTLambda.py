cb0be57c35a03dfe62e1dd12305a275d0961b894
JyanezVilla <jyanez.villa@gmail.com>
2026-06-17 13:18:43 -0600

    Update Lambda.py

--- iotButton_Lambda/IOTLambda.py
+++ iotButton_Lambda/IOTLambda.py
@@ -36,7 +36,7 @@ def deleteNews():
                     }
                     ) 
 
-// Actualización de Lambda
+// ### Actualización de Lambda ###
 def findNews():
     #News credit to newsapi.org
     #Fetch headlines using the API
